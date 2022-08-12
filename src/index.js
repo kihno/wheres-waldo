c0ff7792b64577060e4bd99a0140deff2f5fceb1
kihno <stistyle13@gmail.com>
2022-08-12 12:14:33 -0700

    Deploy gh pages

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import firebase from './utils/firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/wheres-waldo">
       <App />
     </BrowserRouter>
   </React.StrictMode>
